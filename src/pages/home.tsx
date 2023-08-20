370d5ab65536cdc73124098b417493d405f4eabe
Misho <chapidze.misho95@gmail.com>
2023-08-20 16:56:04 +0400

    s

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="w-full min-h-screen bg-neutral-300 flex justify-center items-center">
+      <div className="bg-neutral-400 w-full h-screen sm:w-4/5 sm:h-C_H p-5 shadow-sm rounded-sm">
+        home
+      </div>
+    </div>
+  );
+};
+
+export default Home;
